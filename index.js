8f57263d11fed646a2a2de592fc7dca892bb31b8
TheBeachmaster <otienoken@outlook.com>
2017-05-17 16:38:47 +0300

    Cards

--- index.js
+++ index.js
@@ -32,9 +32,40 @@ app.post('/api/messages', connector.listen());
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')		 
 // Gets the caption by checking the type of the image (stream vs URL) and calling the appropriate caption service method.
 
+// var bot = new builder.UniversalBot(connector, function(session) {
+//     session.send("You said: %s", session.message.text);
+// });
+
+// Create your bot with a function to receive messages from the user
+// Create bot and default message handler
 var bot = new builder.UniversalBot(connector, function(session) {
-    session.send("You said: %s", session.message.text);
+    session.send("Hi... We sell shirts. Say 'show shirts' to see our products.");
 });
+
+// Add dialog to return list of shirts available
+bot.dialog('showShirts', function(session) {
+    var msg = new builder.Message(session);
+    msg.attachmentLayout(builder.AttachmentLayout.carousel)
+    msg.attachments([
+        new builder.HeroCard(session)
+        .title("Classic White T-Shirt")
+        .subtitle("100% Soft and Luxurious Cotton")
+        .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+        .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/whiteshirt.png')])
+        .buttons([
+            builder.CardAction.imBack(session, "buy classic white t-shirt", "Buy")
+        ]),
+        new builder.HeroCard(session)
+        .title("Classic Gray T-Shirt")
+        .subtitle("100% Soft and Luxurious Cotton")
+        .text("Price is $25 and carried in sizes (S, M, L, and XL)")
+        .images([builder.CardImage.create(session, 'http://petersapparel.parseapp.com/img/grayshirt.png')])
+        .buttons([
+            builder.CardAction.imBack(session, "buy classic gray t-shirt", "Buy")
+        ])
+    ]);
+    session.send(msg).endDialog();
+}).triggerAction({ matches: /^(show|list)/i });
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
